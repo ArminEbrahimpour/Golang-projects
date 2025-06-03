2aae47853b625ee837943ba0dcacbe0d1e176789
Rmin <armin890ebp@gmail.com>
2025-06-03 17:48:21 +0330

    writePump finished

--- websockChat/client.go
+++ websockChat/client.go
@@ -19,7 +19,9 @@ type Client struct {
 }
 
 const (
+	writeWait      = 10 * time.Second
 	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
 	maxMessageSize = 512
 )
 
@@ -79,4 +81,43 @@ func (c *Client) readPump() {
 	}
 }
 
-func (c *Client) writePump() {}
+func (c *Client) writePump() {
+
+	ticker := time.NewTicker(pingPeriod)
+
+	defer func() {
+		c.conn.Close()
+	}()
+
+	for {
+		select {
+		case msg, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(msg)
+
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(msg)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+
+	}
+}
